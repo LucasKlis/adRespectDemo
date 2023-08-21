d9e37a7218463780672960eb1d21e643696d7a98
LucasKlis <lukaszklis.poczta@gmail.com>
2023-08-21 18:58:57 +0200

    styling

--- slider.js
+++ slider.js
@@ -28,3 +28,47 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "grid";
   dots[slideIndex-1].className += " active";
 }
+
+const searchIcon = document.querySelector('.inline');
+const searchInput = document.querySelector('input');
+
+searchIcon.addEventListener('click', () => {
+  searchInput.classList.toggle('hidden');
+  searchInput.focus();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const showMoreButton = document.getElementById('showMoreButton');
+  const hiddenImages = document.querySelectorAll('.hidden');
+
+  showMoreButton.addEventListener('click', function() {
+    hiddenImages.forEach(image => {
+      image.classList.remove('hidden');
+    });
+
+    showMoreButton.style.display = 'none';
+  });
+});
+
+const showMoreButton = document.getElementById('showMoreButton');
+const hiddenImages = document.querySelectorAll('.hidden');
+
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".gallery-image");
+  const popup = document.createElement("div");
+  popup.classList.add("image-popup");
+  document.body.appendChild(popup);
+
+  images.forEach((image) => {
+    image.addEventListener("click", () => {
+      const clonedImage = image.cloneNode();
+      popup.innerHTML = "";
+      popup.appendChild(clonedImage);
+      popup.style.display = "flex";
+    });
+  });
+
+  popup.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
+});
